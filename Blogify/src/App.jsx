369c7177d9b93fb3342d003e1d29a919b0245a14
aayushmulani0-jgp <aayushmulani0@gmail.com>
2026-04-06 14:39:23 +0530

    fixed new blog page with pagination and filters

--- Blogify/src/App.jsx
+++ Blogify/src/App.jsx
@@ -4,21 +4,25 @@ import Dashboard from "./Components/Dashboard";
 import About from "./Components/About";
 import Contact from "./Components/Contact";
 import NotFound from "./Site/NotFound";
-import Blog from "./Components/Blog";
+import Blog1 from "./Components/Blog1";
 import Home from "./Components/Home";
+import Home1 from "./Components/Home1";
+import Blog from "./Components/Blog";
 import "./App.css";
 
 function App() {
   return (
     <Routes>
+      <Route path="home" element={<Home1 />} />
+      <Route path="blog1" element={<Blog1 />} />
+      {/* <Route path="/" element={<Home1 />} /> */}
       <Route path="/" element={<ProLayout />}>
-        <Route index element={<Dashboard />} />
         <Route path="dashboard" element={<Dashboard />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="home" element={<Home />} />
         <Route path="blog" element={<Blog />} />
+        <Route path="old-home" element={<Home />} />
       </Route>
     </Routes>
   );
